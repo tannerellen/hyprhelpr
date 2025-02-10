5ff8264d6812bc1bbc0101fc0e999e0d44689ea3
Tanner Ellen <tanner@seedcode.com>
2025-02-09 23:36:27 -0800

    refactor: change build to use cpu optimizations

--- build.sh
+++ build.sh
@@ -2,4 +2,4 @@
 
 mkdir -p ./bin
 
-bun build --compile --minify --sourcemap --bytecode --target=bun-linux-x64-baseline ./index.js --outfile bin/hyprhelpr
+bun build --compile --minify --sourcemap --bytecode --target=bun-linux-x64 ./index.js --outfile bin/hyprhelpr
